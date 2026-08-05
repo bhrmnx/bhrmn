2346144d5e5f297d2c2580fd0ecd9eca9457cf4a
Yash Thakur <yashthakur@Yashs-MacBook-Air.local>
2026-08-05 20:22:00 +0530

    Initial Expo app running on device

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Bhrmn</Text>
+      <Text>Your Travel Social Identity</Text>
       <StatusBar style="auto" />
     </View>
   );
